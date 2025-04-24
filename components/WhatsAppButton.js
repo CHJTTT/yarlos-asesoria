a5abd0d791e188ce5bd0d4096b9e14794aa25aff
CHJTTT <christianchinchaytomas@gmail.com>
2025-04-24 11:55:53 -0500

    Feat: Añadir número correcto a WhatsAppButton

--- components/WhatsAppButton.js
+++ components/WhatsAppButton.js
@@ -5,7 +5,7 @@ import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 export default function WhatsAppButton() {
   return (
     <a
-    href="https://wa.me/51961678632"
+    href="https://wa.me/51956498610"
     target="_blank"
     rel="noopener noreferrer"
     className={styles.whatsappButton} // Aquí la corrección
